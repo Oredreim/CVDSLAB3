03c4beebd6f59daec7c2fa429372ee1653c596be
Oredreim <oredreim@github.com>
2021-02-10 10:20:10 -0500

    solo faltan pruebas

---
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -0,0 +1,9 @@
+package edu.eci.cvds.tdd.aerodescuentos;
+
+import org.junit.Assert;
+import org.junit.Test;
+import edu.eci.cvds.tdd.aerodescuentos.*;
+
+public class TarifasTest {
+
+}
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -7,16 +7,51 @@ public class RegistryTest {
     private Registry registry = new Registry();
 
     @Test
-    public void validateLowerLimitInvalidAge() {
-        Person prueba = new Person("Cristian", 1111111, -5, Gender.MALE , true);
+    public void validaredadMenorACero() {
+        Person prueba = new Person("Cristian", 1234567, -5, Gender.MALE , true);
         RegisterResult result = registry.registerVoter(prueba);
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
 
     @Test
-    public void validateUpperLimitInvalidAge() {
-        Person prueba = new Person("Cristian", 1111111, 201, Gender.MALE , true);
+    public void validarEdadMaxima() {
+        Person prueba = new Person("Cristian", 1234567, 201, Gender.MALE , true);
         RegisterResult result = registry.registerVoter(prueba);
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    @Test
+    public void validarMenorDeEdad() {
+        Person prueba = new Person("Cristian", 1234567, 17, Gender.MALE , true);
+        RegisterResult result = registry.registerVoter(prueba);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void validarMuerto() {
+        Person prueba = new Person("Cristian", 1234567, 28, Gender.MALE , false);
+        RegisterResult result = registry.registerVoter(prueba);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    @Test
+    public void validarDuplicado() {
+        Person prueba = new Person("Pepito", 1111111, 28, Gender.MALE , true);
+        RegisterResult result = registry.registerVoter(prueba);
+        Assert.assertEquals(RegisterResult.DUPLICATED, result);
+    }
+
+    @Test
+    public void validarVivo() {
+        Person prueba = new Person("Cristian", 231546, 28, Gender.MALE , true);
+        RegisterResult result = registry.registerVoter(prueba);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+
+    @Test
+    public void validarNoDuplicado() {
+        Person prueba = new Person("Pepito", 22222, 28, Gender.MALE , true);
+        RegisterResult result = registry.registerVoter(prueba);
+        Assert.assertEquals(RegisterResult.VALID, result);
     }
 }
