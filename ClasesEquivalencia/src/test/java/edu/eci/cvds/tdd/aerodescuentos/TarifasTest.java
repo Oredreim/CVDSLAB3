c1d8b86d971804c094f866b82c94a19433ba8b3d
Oredreim <oredreim@github.com>
2021-02-11 09:48:12 -0500

    lab terminado

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -5,72 +5,103 @@ import org.junit.Test;
 import edu.eci.cvds.tdd.aerodescuentos.*;
 
 public class TarifasTest {
-	private CalculadorDescuentos calculo;
+
+	private double tarifa;
+
 	@Test
-	public void Dias20 () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos();
-		Assert.assertEquals(calculo.calculoTarifa(10000,21,20),7500);
+	public void validarEdadMenor0(){
+		try{
+			tarifa = CalculadorDescuentos.calculoTarifa(5000000,15,-100);
+		}
+		catch ( Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void menores18 () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		Assert.assertEquals(calculo.calculoTarifa(10000,12,16),9500);
+	public void validarEdad0(){
+		try{
+			tarifa = CalculadorDescuentos.calculoTarifa(5000000,15,0);
+		}
+		catch ( Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void mayores65 () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		Assert.assertEquals(calculo.calculoTarifa(10000,12,68),9200);
+	public void menores18 () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000, 12, 16);
+		}
+		catch (Exception e) {
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void mayores65YDias20 () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		Assert.assertEquals(calculo.calculoTarifa(10000,21,68),7700);
+	public void mayores65 () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000, 12, 68);
+		}
+		catch (Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void menores18YDias20 () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		Assert.assertEquals(calculo.calculoTarifa(10000,21,68),8000);
+	public void mayores65YDias20 () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000,21,68);
+		} catch (Exception e) {
+			Assert.assertTrue(true);
+		}
+
+	}
+
+	@Test
+	public void menores18YDias20 () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000, 21, 68);
+		}
+		catch (Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 
 	@Test
-	public void noDeberiaDescuento () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		Assert.assertEquals(calculo.calculoTarifa(10000,5,24),10000);
+	public void noDeberiaDescuento () {
+		tarifa = CalculadorDescuentos.calculoTarifa(10000,5,24);
 	}
 
 	@Test
-	public void edadErronea () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		try { calculo.calculoTarifa(10000,5,-24);}
-			catch(ExcepcionParametrosInvalidos e){
-				Assert.assertTrue(true);
-			}
+	public void edadErronea () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000,5,-24);
+		}
+		catch(Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void diasErroneos () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		try { calculo.calculoTarifa(10000,-5,24);}
-			catch(ExcepcionParametrosInvalidos e){
-				Assert.assertTrue(true);
-			}
+	public void diasErroneos () {
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(10000,-5,24);
+		}
+		catch(Exception e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
-	public void precioErroneo () throws ExcepcionParametrosInvalidos{
-		calculo = new CalculadorDescuentos;
-		try { calculo.calculoTarifa(-10000,5,24);}
-			catch(ExcepcionParametrosInvalidos e){
-				Assert.assertTrue(true);
-			}
+	public void precioErroneo () {
+
+		try {
+			tarifa = CalculadorDescuentos.calculoTarifa(-10000,5,24);
+		}
+		catch(Exception e){
+			Assert.assertTrue(true);
+		}
 	}
-
-
-
-
 }
